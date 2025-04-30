6041cbe2ebff26b5015e7b59509eea2d6a6a162c
pabitra <89703515+appu900@users.noreply.github.com>
2025-04-30 10:41:39 +0530

    added cors

--- src/expressApp.ts
+++ src/expressApp.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import AdminRoutes from "./api/admin.routes";
 import JobRoutes from "./api/job.routes";
-import UserRoutes from "./api/user.routes"
-import JobApplicationRoutes from "./api/application.routes"
+import UserRoutes from "./api/user.routes";
+import JobApplicationRoutes from "./api/application.routes";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/admin", AdminRoutes);
 app.use("/api/job", JobRoutes);
-app.use("/api/user",UserRoutes)
-app.use("/api/application",JobApplicationRoutes)
+app.use("/api/user", UserRoutes);
+app.use("/api/application", JobApplicationRoutes);
 
 export default app;
